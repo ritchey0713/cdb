d8bbf151af3a2a2c75a9d06857f85a06fba33622
ritchey0713 <ritchey0713@gmail.com>
2020-03-01 14:09:49 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learndb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
